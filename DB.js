7d729c71f401ae215631cf58c6e0eaa6378ded9b
Sandy Chan <Sandy.chan7@studytafensw.edu.au>
2026-01-31 23:38:15 +1100

    debugging connection error a2

--- DB.js
+++ DB.js
@@ -246,3 +246,21 @@ export async function postResults(setID, HCW, Moment, Action, Glove,
     throw error;
   }
 }
+
+app.get('/env-check', (req, res) => {
+  res.json({
+    hasDbUser: !!process.env.DB_USER,
+    hasDbServer: !!process.env.DB_SERVER,
+  });
+});
+
+app.get('/db-test', async (req, res) => {
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request().query('SELECT 1 AS ok');
+    res.json(result.recordset);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
