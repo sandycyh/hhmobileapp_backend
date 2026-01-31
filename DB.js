1dca4e605d82d25ab95fd5d54a559a2e2a317245
Sandy Chan <Sandy.chan7@studytafensw.edu.au>
2026-01-31 23:52:09 +1100

    debugging connection a4

--- DB.js
+++ DB.js
@@ -247,20 +247,20 @@ export async function postResults(setID, HCW, Moment, Action, Glove,
   }
 }
 
-app.get('/env-check', (req, res) => {
-  res.json({
-    hasDbUser: !!process.env.DB_USER,
-    hasDbServer: !!process.env.DB_SERVER,
-  });
-});
+// app.get('/env-check', (req, res) => {
+//   res.json({
+//     hasDbUser: !!process.env.DB_USER,
+//     hasDbServer: !!process.env.DB_SERVER,
+//   });
+// });
 
-app.get('/db-test', async (req, res) => {
-  try {
-    const pool = await poolPromise;
-    const result = await pool.request().query('SELECT 1 AS ok');
-    res.json(result.recordset);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json(err);
-  }
-});
+// app.get('/db-test', async (req, res) => {
+//   try {
+//     const pool = await poolPromise;
+//     const result = await pool.request().query('SELECT 1 AS ok');
+//     res.json(result.recordset);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json(err);
+//   }
+// });
--- server.js
+++ server.js
@@ -35,20 +35,20 @@ app.use('/api/ResultSets', resultSetsRoutes)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Running on ${PORT}`));
 
-// app.get('/env-check', (req, res) => {
-//   res.json({
-//     hasDbUser: !!process.env.DB_USER,
-//     hasDbServer: !!process.env.DB_SERVER,
-//   });
-// });
+app.get('/env-check', (req, res) => {
+  res.json({
+    hasDbUser: !!process.env.DB_USER,
+    hasDbServer: !!process.env.DB_SERVER,
+  });
+});
 
-// app.get('/db-test', async (req, res) => {
-//   try {
-//     const pool = await poolPromise;
-//     const result = await pool.request().query('SELECT 1 AS ok');
-//     res.json(result.recordset);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json(err);
-//   }
-// });
+app.get('/db-test', async (req, res) => {
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request().query('SELECT 1 AS ok');
+    res.json(result.recordset);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
